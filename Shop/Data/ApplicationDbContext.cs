debc10ede72ef055a41723e0f32a464f4e6ec77c
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-18 19:14:37 +0300

    Created category table

--- Shop/Data/ApplicationDbContext.cs
+++ Shop/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
 
 namespace Shop.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
         {
