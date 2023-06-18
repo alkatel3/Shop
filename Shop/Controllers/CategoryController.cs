c2ecc2a98f0c8ac956296115e6c222b98154c733
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-18 19:54:56 +0300

    Inplemented getting all categories from db

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models;
 
 namespace Shop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> Categories = db.Categories.ToList();
             return View();
         }
     }
