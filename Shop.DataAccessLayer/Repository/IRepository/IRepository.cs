b529095f832d3a95f79125fa47798617bbd3c02c
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-23 14:43:08 +0300

    Created generic repository

--- Shop.DataAccessLayer/Repository/IRepository/IRepository.cs
+++ Shop.DataAccessLayer/Repository/IRepository/IRepository.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace Shop.DataAccessLayer.Repository.IRepository
 {
-    internal interface IRepository
+    public interface IRepository<T> where T : class
     {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool> >filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
     }
 }
---
+++ Shop.DataAccessLayer/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.DataAccessLayer.Data;
+using Shop.DataAccessLayer.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace Shop.DataAccessLayer.Repository
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly ApplicationDbContext db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            this.db = db;
+            dbSet = db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+    }
+}
