82df734c12d8bad40331b50fc5bfe8f10191b246
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-23 14:54:29 +0300

    Created Category repository

---
+++ Shop.DataAccessLayer/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using Shop.DataAccessLayer.Data;
+using Shop.DataAccessLayer.Repository.IRepository;
+using Shop.Models;
+
+namespace Shop.DataAccessLayer.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext db;
+        
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            this.db = db;
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            db.Categories.Update(category);
+        }
+    }
+}
