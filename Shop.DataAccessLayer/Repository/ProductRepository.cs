f63fa2ce0cd6f06a0d00446466ccb0e2a09f9a03
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-27 18:47:38 +0300

    Added update Product custom code

--- Shop.DataAccessLayer/Repository/ProductRepository.cs
+++ Shop.DataAccessLayer/Repository/ProductRepository.cs
@@ -15,7 +15,22 @@ namespace Shop.DataAccessLayer.Repository
 
         public void Update(Product product)
         {
-            dbSet.Update(product);
+            var productDb = db.Products.FirstOrDefault(u => u.Id == product.Id);
+            if(productDb != null)
+            {
+                productDb.Title = product.Title;
+                productDb.ISBN = product.ISBN;
+                productDb.Price = product.Price;
+                productDb.Price50 = product.Price50;
+                productDb.Price100 = product.Price100;
+                productDb.Description = product.Description;
+                productDb.CategoryId = product.CategoryId;
+                productDb.Author = product.Author;
+                if(product.ImageUrl != null)
+                {
+                    productDb.ImageUrl = product.ImageUrl;
+                }
+            }
         }
     }
 }
