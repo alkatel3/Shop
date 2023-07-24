6dd6a5bef16ca19bc9c3f0efe11f6f33375e8355
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-07-24 15:56:04 +0300

    Implemented based shopping cart UI

--- Shop.DataAccessLayer/Repository/IRepository/IRepository.cs
+++ Shop.DataAccessLayer/Repository/IRepository/IRepository.cs
@@ -5,7 +5,7 @@ namespace Shop.DataAccessLayer.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter=null, string? includeProperties = null);
         T Get(Expression<Func<T, bool> >filter, string? includeProperties = null, bool tracked=false);
         void Add(T entity);
         void Delete(T entity);
---
+++ Shop/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.DataAccessLayer.Repository.IRepository;
+using Shop.Models;
+using Shop.Models.ViewModels;
+using System.Security.Claims;
+
+namespace Shop.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork UoW;
+        public ShoppingCartVM ShoppingCartVM { get; set; } 
+
+        public CartController( IUnitOfWork unitOfWork)
+        {
+            UoW = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = UoW.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")  
+            };
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                double price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += price * cart.Count;
+            }
+            return View(ShoppingCartVM);
+        }
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppincCart)
+        {
+            if (shoppincCart.Count <= 50)
+            {
+                return shoppincCart.Product.Price;
+            }
+            else if (shoppincCart.Count <= 100)
+            {
+                return shoppincCart.Product.Price50;
+            }
+            else
+            {
+                return shoppincCart.Product.Price100;
+            }
+        }
+    }
+}
