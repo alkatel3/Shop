ed0bc7dae9184f657c554be9371e49c22d781c98
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-07-29 02:25:10 +0300

    Filters In Order List

--- Shop.Utility/SD.cs
+++ Shop.Utility/SD.cs
@@ -9,7 +9,7 @@
 
         public const string StatusPending = "Pending";
 		public const string StatusApproved = "Approved";
-		public const string StatusProcessing = "Processing";
+		public const string StatusInProcess = "Processing";
 		public const string StatusShipped = "Shipped";
 		public const string StatusCancelled = "Cancelled";
 		public const string StatusRefunded = "Refunded";
--- Shop/Areas/Admin/Controllers/OrderController.cs
+++ Shop/Areas/Admin/Controllers/OrderController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Shop.DataAccessLayer.Repository;
 using Shop.DataAccessLayer.Repository.IRepository;
 using Shop.Models;
+using Shop.Utility;
+using System.Diagnostics;
 
 namespace Shop.Areas.Admin.Controllers
 {
@@ -23,10 +25,29 @@ namespace Shop.Areas.Admin.Controllers
 
 		#region API CALLS
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> orderHeaders = UoW.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-			return Json(new { data = orderHeaders });
+			IEnumerable<OrderHeader> orderHeaders = UoW.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+					orderHeaders = orderHeaders.Where(o => o.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(o => o.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(o => o.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(o => o.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { data = orderHeaders });
 		}
 		#endregion
 	}
