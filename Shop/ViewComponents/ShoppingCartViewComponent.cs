7e59d329a01a3bdf40345e995ec4c24ba3edef48
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-08-01 15:13:45 +0300

    ViewComponent in Action

--- Shop/ViewComponents/ShoppingCartViewComponent.cs
+++ Shop/ViewComponents/ShoppingCartViewComponent.cs
@@ -1,6 +1,38 @@
-﻿namespace Shop.ViewComponents
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.DataAccessLayer.Repository.IRepository;
+using Shop.Utility;
+using System.Security.Claims;
+
+namespace Shop.ViewComponents
 {
-    public class ShoppingCartViewComponent
+    public class ShoppingCartViewComponent : ViewComponent
     {
+        private readonly IUnitOfWork UoW;
+        
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            UoW = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                    UoW.ShoppingCart.GetAll(c => c.ApplicationUserId == claim.Value).Count());
+                }
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
     }
 }
